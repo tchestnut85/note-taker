b94d18cc4b8ce252354ef47e9c045840460e1ffa
Tom Chestnut <tchestnut85@gmail.com>
2020-10-14 22:08:17 -0400

    organized directories

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ app.get('/api/db/', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 app.listen(PORT, () => {
