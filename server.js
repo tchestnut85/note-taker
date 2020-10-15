f553a1b998c3bdc0437bb903a1438e2745d8e35e
Tom Chestnut <tchestnut85@gmail.com>
2020-10-15 11:55:06 -0400

    created API GET route for notes saved in db.json

--- server.js
+++ server.js
@@ -3,20 +3,23 @@ const PORT = process.env.PORT || 3002;
 const app = express();
 const fs = require('fs');
 const path = require('path');
-const { db } = require('./db/db.json');
+const { notes } = require('./db/db.json');
 
-app.get('/api/db/', (req, res) => {
-    res.send('Hi there!');
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
 });
 
+// GET index html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+// GET notes html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
+// run the server
 app.listen(PORT, () => {
     console.log(`Note-Taker API server is now on port ${PORT}!`);
 });
